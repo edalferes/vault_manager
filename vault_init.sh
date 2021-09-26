43547f7ff1d7c86ab95985587f85fae13afb0983
Edmilson Alferes <edmilson.alferes@alpheres.com>
2021-09-26 18:22:02 -0300

    commit inicial

---
+++ vault_init.sh
@@ -0,0 +1,72 @@
+
+#!/bin/bash
+#
+# Description: Script for install and configure a single HashiCorp Vault cluster
+# Maintainer: Edmilson Alferes <edmilson.alferes@alpheres.com>
+
+# Global Variables
+LOG_FILE="vault.log"
+
+
+# Use log info <function> <msg>
+log_info(){
+
+	TEXT_COLOR_INIT="\e[0;32m"
+    TEXT_COLOR_FINAL="\e[0m"
+	DT=$(date "+%Y/%m/%d %H:%M:%S")
+	STR_CONSOLE="${TEXT_COLOR_INIT} [${DT}] - [INFO] - [${1}]: ${2} ${TEXT_COLOR_FINAL}"
+    STR_FILE="[${DT}] - [INFO] - [${1}]: ${2}"
+    echo -e ${STR_CONSOLE}
+    echo ${STR_FILE} >> ${LOG_FILE}
+}
+
+# Use log warning <function> <msg>
+log_warning(){
+
+	TEXT_COLOR_INIT="\e[1;33m"
+    TEXT_COLOR_FINAL="\e[0m"
+	DT=$(date "+%Y/%m/%d %H:%M:%S")
+	STR_CONSOLE="${TEXT_COLOR_INIT} [${DT}] - [INFO] - [${1}]: ${2} ${TEXT_COLOR_FINAL}"
+    STR_FILE="[${DT}] - [INFO] - [${1}]: ${2}"
+    echo -e ${STR_CONSOLE}
+    echo ${STR_FILE} >> ${LOG_FILE}
+}
+
+# Use log error <function> <msg>
+log_error(){
+
+	TEXT_COLOR_INIT="\e[1;31m"
+    TEXT_COLOR_FINAL="\e[0m"
+	DT=$(date "+%Y/%m/%d %H:%M:%S")
+	STR_CONSOLE="${TEXT_COLOR_INIT} [${DT}] - [INFO] - [${1}]: ${2} ${TEXT_COLOR_FINAL}"
+    STR_FILE="[${DT}] - [INFO] - [${1}]: ${2}"
+    echo -e ${STR_CONSOLE}
+    echo ${STR_FILE} >> ${LOG_FILE}
+}
+
+
+
+install_vault() {
+
+	log_info "install_vault" "Install Vault"
+
+
+}
+
+configure_path_permissions() {
+
+	log_info "configure_path_permissions" "Configure path and permissions"
+
+
+
+}
+
+
+create_certificate() {
+	log_info "create_certificate" "Create Certificate self signed"
+
+}
+
+main() {
+    log_info "main" "test_log"
+}
